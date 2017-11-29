9f3b0bdea1d2c2ed71abe0f48769bfd5af61ff85
Sergio Ocón <sergio.ocon@redhat.com>
2017-11-29 13:25:31 +0100

    Update config

--- src/server.js
+++ src/server.js
@@ -52,8 +52,8 @@ app.get('*', (req, res) => {
 });
 
 // start the server
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-const env = process.env.NODE_ENV || 'production';
+var server_port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+const env = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 server.listen(server_port,server_ip_address, err => {
   if (err) {
