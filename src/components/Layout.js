db9f9e05e55c339835acb3d9f3c86ac45b94d7fd
Alberto Gutierrez <aljesusg@gmail.com>
2017-12-05 09:41:11 +0100

    console

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,10 +2,29 @@
 
 import React from 'react';
 import Menu from './Nav/Menu';
+import axios from 'axios';
 
 const urlForApiVersion =`${ process.env.API_BACKEND}/api/version`
 
 export default class Layout extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {version: '_'};
+  }
+
+  componentDidMount() {
+    console.log(process.env.API_BACKEND)
+    console.log(process.env.GITHUB_OAUTH_ID)
+    console.log(process.env.GITHUB_REDIRECTUI)
+    axios.get(process.env.API_BACKEND)
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
   render() {
     return (
       <div className="app-container">
@@ -19,6 +38,9 @@ export default class Layout extends React.Component {
           { process.env.GITHUB_REDIRECTUI }
             This is a demo app to showcase manageiq galaxy web with <strong>React</strong> and <strong>Express</strong>.
           </p>
+          <p>
+            Version { this.state.version }
+          </p>  
           <p>
             Contribute on <a href="https://github.com/miq-consumption/manageiq-galaxy-web">ManageIQ Galaxy Repo</a>
           </p>
